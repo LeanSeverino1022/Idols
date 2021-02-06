60ec6a8db77aac8b853730389f826950ce9a1649
Leandro Severino Jr <yihseverino@gmail.com>
2021-02-06 23:35:48 +0800

    Add cards and filter feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+
+// I refer to the items as monsters. Not sure yet if I'm going to change this to people or whatever, I just want a filterable sample to get started.
+
 // import logo from './logo.svg';
 import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -8,27 +14,39 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
-    
+
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then( response => response.json())
-    .then( users => this.setState({ monsters: users }))
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   }
 
-  render () {
+  render() {
+
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter( monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
-      <div className="App">
-        {
-          this.state.monsters.map( monster => <h1 key={monster.id}> {  monster.name } </h1>)
-        }
+      <div className="App" style={{ background: '#F1F3F5' }}>
+        <input
+          type='search'
+          placeholder='Search name'
+          onChange={e =>  this.setState({ searchField: e.target.value }) }
+        />
+        <CardList monsters={filteredMonsters}>
+        </CardList>
       </div>
     );
   }
-  
+
 }
 
 export default App;
